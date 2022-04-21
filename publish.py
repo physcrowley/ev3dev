c5923b462d760d3db5a37e792271a32c85547291
physcrowley <crowlda@ecolecatholique.ca>
2022-04-20 21:31:18 -0400

    removed line-ending ;s

--- publish.py
+++ publish.py
@@ -8,5 +8,5 @@ import paho.mqtt.client as mqtt
 
 client = mqtt.Client()
 client.connect("localhost",1883,60)
-client.publish("topic/test", "Hello world!");
-client.disconnect();
+client.publish("topic/test", "Hello world!")
+client.disconnect()
